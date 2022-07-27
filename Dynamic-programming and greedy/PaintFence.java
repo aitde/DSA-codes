a9300bebb34c4d67143fb8fa55e61350b496ace8
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-07-27 22:19:44 +0530

    Add files via upload

--- Dynamic-programming and greedy/PaintFence.java
+++ Dynamic-programming and greedy/PaintFence.java
@@ -6,25 +6,31 @@ class PaintFence {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int ans = getWays(n, k, -1, 2);
+        int[][][] dp = new int[n + 1][k + 1][3];
+        int ans = getWays(n, k, -1, 2, dp);
         System.out.println(ans);
         sc.close();
     }
 
-    public static int getWays(int n, int k, int prevc, int count) {
+    static int getWays(int n, int k, int prevc, int count, int[][][] dp) {
         if (n == 0)
             return 1;
+
+        if (prevc != -1 && dp[n][prevc][count] != 0)
+            return dp[n][prevc][count];
 
         int ans = 0;
 
         for (int i = 0; i < k; i++) {
             if (i == prevc) {
                 if (count < 2)
-                    ans += getWays(n - 1, k, i, 2);
+                    ans += getWays(n - 1, k, i, 2, dp);
             } else {
-                ans += getWays(n - 1, k, i, 1);
+                ans += getWays(n - 1, k, i, 1, dp);
             }
         }
+        if (prevc != -1)
+            dp[n][prevc][count] = ans;
         return ans;
     }
 }
