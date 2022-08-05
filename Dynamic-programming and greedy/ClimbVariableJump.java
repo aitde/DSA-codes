96d3a2cea007c9a3d0949085f5ce33c43ebe279b
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-08-05 11:31:27 +0530

    Update ClimbVariableJump.java

--- Dynamic-programming and greedy/ClimbVariableJump.java
+++ Dynamic-programming and greedy/ClimbVariableJump.java
@@ -1,35 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ClimbVariableJump {
+
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        
+        for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-
-        int ans = Varjump(arr, 0, n, new int[n + 1]);
-        System.out.println(ans);
-        sc.close();
+        
+        
+        int[] dp = new int[n+1];
+        dp[n]=1;
+        
+        for(int i=n-1; i>=0; i--){
+            int ans = 0;
+            for(int j=1; j<=arr[i]; j++){
+                if((i+j<=n) && arr[i]!=0){
+                    ans += dp[i+j];
+                }
+            }
+            dp[i] = ans;
+        }
+        
+        
+        System.out.println(dp[0]);
     }
 
-    public static int Varjump(int[] arr, int i, int n, int[] dp) {
-        if (i == n)
-            return 1;
-        if (i > n)
-            return 0;
-
-        if (dp[i] != 0) {
-            return dp[i];
-        }
-        int sum = 0;
-
-        for (int j = 1; j <= arr[i]; j++) {
-            sum += Varjump(arr, i + j, n, dp);
-        }
-        dp[i] = sum;
-        return dp[i];
-    }
 }
