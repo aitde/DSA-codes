4597185c7ddcbf15d03eaf7afdc72a4caf3a2b7c
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-08-05 10:49:04 +0530

    Update FibonacciDP.java

--- Dynamic-programming and greedy/FibonacciDP.java
+++ Dynamic-programming and greedy/FibonacciDP.java
@@ -1,27 +1,21 @@
+
 import java.util.*;
 
 public class FibonacciDP {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        int ans = fibDp(n, new int[n + 1]);
-        System.out.println(ans);
-        sc.close();
+public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
+    int[] dp = new int[n+1];
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-2]+dp[i-1];
     }
-
-    public static int fibDp(int n, int[] dp) {
-        if (n == 0 || n == 1)
-            return n;
-
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-
-        int x = fibDp(n - 1, dp);
-        int y = fibDp(n - 2, dp);
-
-        dp[n] = x + y;
-        return dp[n];
-    }
+    System.out.println(dp[n]);
+ }
+    
 }
