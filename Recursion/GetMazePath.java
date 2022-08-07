bea276fc03b3720091d6eebb4f0c1d8e888bd27a
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-08-07 16:50:06 +0530

    Add files via upload

--- Recursion/GetMazePath.java
+++ Recursion/GetMazePath.java
@@ -1,45 +1,38 @@
-import java.io.*;
+
 import java.util.*;
 
-public class GetMazePath {
+public class GetMazePaths {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        
-        ArrayList<String> ans = getMazePaths(0, 0, n-1, m-1);
+
+        getMazePaths(0, 0, n - 1, m - 1, "");
         System.out.println(ans);
+        sc.close();
     }
+
+    static ArrayList<String> ans = new ArrayList<>();
 
     // sr - source row
     // sc - source column
     // dr - destination row
     // dc - destination column
-    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
-        if(sr == dr && sc == dc){
-            ArrayList<String> al = new ArrayList<>();
-            al.add("");
-            return al;
+    public static void getMazePaths(int sr, int sc, int dr, int dc, String path) {
+        if ((sr == dr) && (sc == dc)) {
+            ans.add(path);
+            return;
         }
-        
-        ArrayList<String> ans = new ArrayList<>();
-        
-        if((sc+1) <= dc){
-            ArrayList<String> partAns = getMazePaths(sr, sc+1, dr, dc);
-        
-        for(int i=0; i<partAns.size(); i++){
-            ans.add('h' + partAns.get(i));
+
+        if (sr + 1 <= dr) {
+            getMazePaths(sr + 1, sc, dr, dc, 'v' + path);
         }
+
+        if (sc + 1 <= dc) {
+            getMazePaths(sr, sc + 1, dr, dc, 'h' + path);
         }
-        
-        if((sr+1) <=dr){
-        ArrayList<String> partAns = getMazePaths(sr+1, sc, dr, dc);
-        for(int i=0; i<partAns.size(); i++){
-            ans.add('v' + partAns.get(i));
-        }
-        }
-        return ans;
+
     }
 
 }
