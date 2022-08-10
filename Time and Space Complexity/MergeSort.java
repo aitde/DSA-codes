b8cfd305b0725bba84c94de0e4b03483d51f33c5
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-08-10 12:19:17 +0530

    Add files via upload

--- Time and Space Complexity/MergeSort.java
+++ Time and Space Complexity/MergeSort.java
@@ -1,76 +1,64 @@
-import java.io.*;
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 import java.util.*;
 
-public class MergeSort {
-
-  public static int[] mergeSort(int[] arr, int lo, int hi) {
-     if(lo==hi){
-       int[] ar = new int[1];
-       ar[0] = arr[lo];
-       return ar;
-     }
-     
-     int mid = (hi+lo)/2;
-
-     int[] left = mergeSort(arr, lo, mid);
-     int[] right = mergeSort(arr, mid+1, hi);
-
-     return mergeTwoSortedArrays(left, right);
-  }
-
-  //used for merging two sorted arrays
-  public static int[] mergeTwoSortedArrays(int[] a, int[] b){
-    System.out.println("Merging these two arrays ");
-    System.out.print("left array -> ");
-    print(a);
-    System.out.print("right array -> ");
-    print(b);
-    int i = 0, j =0, k = 0;
-    int[] ans = new int[a.length + b.length];
-    while(i < a.length && j < b.length){
-        if(a[i] <= b[j]){
-          ans[k] = a[i];
-          i++;
-          k++;
-        }else{
-          ans[k] = b[j];
-          j++;
-          k++;
-        }
-    }
-
-    while(i < a.length){
-      ans[k] = a[i];
-      k++;
-      i++;
-    }
-
-    while(j < b.length){
-      ans[k] = b[j];
-      k++;
-      j++;
-    }
-    
-    return ans;
-  }
-
-  public static void print(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
-    }
-    System.out.println();
-  }
-
-  public static void main(String[] args) throws Exception {
-    Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
-      arr[i] = scn.nextInt();
-    }
-    int[] sa = mergeSort(arr,0,arr.length - 1);
-    System.out.print("Sorted Array -> ");
-    print(sa);
-  }
-
+public class mergeSort
+{
+	public static void main(String[] args) {
+		int[] arr = {5,243,465,0,2,8,-1};
+		arr = mergeSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	public static int[] mergeSort(int[] arr){
+	    if(arr.length == 0){
+	        return arr;
+	    }
+	    if(arr.length==1){
+	        return arr;
+	    }
+	    
+	    int mid = arr.length/2;
+	    
+	    int[] firstHalf = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+	    int[] secondHalf = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+	    
+	    return mergeSorted(firstHalf, secondHalf);
+	}
+	public static int[] mergeSorted(int[] a, int[] b){
+	    int[] ans = new int[a.length+b.length];
+	    
+	    int i=0, j=0, k=0;
+	    
+	    while(i<a.length && j<b.length){
+	        if(a[i]<b[j]){
+	            ans[k] = a[i];
+	            k++;
+	            i++;
+	        }
+	        else{
+	            ans[k] = b[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	    
+	    while(i<a.length){
+	        ans[k] = a[i];
+	            k++;
+	            i++;
+	    }
+	    
+	    while(j<b.length){
+	        ans[k] = b[j];
+	            j++;
+	            k++;
+	    }
+	    return ans;
+	}
 }
