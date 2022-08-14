179059b86a16074f892d18904671ce8db5b22c67
afzal mehndi <90967381+aitde@users.noreply.github.com>
2022-08-14 18:54:42 +0530

    Create DecimalToAnyBase.java

---
+++ Function and Arrays/DecimalToAnyBase.java
@@ -0,0 +1,26 @@
+import java.util.*;
+  
+  public class DecimalToAnyBase {
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int dn = getValueInBase(n, b);
+      System.out.println(dn);
+   }
+  
+   public static int getValueInBase(int n, int b){
+       int ans = 0;
+       int mul = 1;
+       
+       while(n!=0){
+           int rem = n%b;
+           ans += rem*mul;
+           
+           mul = mul*10;
+           n=n/b;
+       }
+       return ans;
+   }
+  }
